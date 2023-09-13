f256bfbe33dfa52d19d9392d0fdc7bde41142aa9
skatksdnlqndjddl <skatksdnlqndjddl@naver.com>
2023-09-13 15:14:30 +0900

    week2

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,48 +1,50 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <conio.h>
-#include <string>
 
 int main()
 {
-	printf("이은석의 VC++");
+    printf("이은석의 VC++\n");
 
-	char code;
-	char exit;
+    char code;
+    char exit;
 
-	while (1) {
-		printf("command>");
-		while (1)
-		{
-			code = _getche();
-			if (code == 27) {
-				return(0);
-			}
-			if (code == 13) {
-				break;
-			}
-		}
-		printf("\n");
+    while (1) {
+        printf("command>");
+        code = _getche();
 
-		switch (code) {
-		case 'q':
-			printf("q : qwer 화이팅\n");
-			break;
-		case 'w':
-			printf("q : qwer 화이팅\n");
-			break;
-		case 'e':
-			printf("q : qwer 화이팅\n");
-			break;
-		case 'r':
-			printf("q : qwer 화이팅\n");
-			break;
-		default:
-			printf("에러\n");
-			break;
+        if (code == 27) {
+            return(0);
+        }
 
-		}
-	}
-	return 0;
+        printf("\n");
+
+        if (code == 13) {
+            continue; 
+        }
+
+        while (true) {
+            char enter = _getch();
+            if (enter == '\r') 
+                break;
+        }
+
+        switch (code) {
+        case 'q':
+            printf("q : qwer 화이팅\n");
+            break;
+        case 'w':
+            printf("w :  과제 너무 좋다\n");
+            break;
+        case 'e':
+            printf("e : 담주부턴 과제량 3ㅂ..네?\n");
+            break;
+        case 'r':
+            printf("r : 행복합니다\n");
+            break;
+        default:
+            printf("에러\n");
+            break;
+        }
+    }
+    return 0;
 }
-	
